ec6d8b573e74067f61ad94202fa7c1cadf0dee6f
martin byrne <marb61a@gmail.com>
2016-04-17 17:00:10 +0000

    added initial app.js create class function

--- src/js/components/App.js
+++ src/js/components/App.js
@@ -10,7 +10,26 @@ function getAppState(){
 }
 
 var App = React.createClass({
-    
+    getInitialState: function(){
+		return getAppState();
+	},
+
+	componentDidMount: function(){
+		AppStore.addChangeListener(this._onChange);
+	},
+
+	componentUnmount: function(){
+		AppStore.removeChangeListener(this._onChange);
+	},
+	
+	render : function(){
+		
+	},
+	
+	// Update view state when change is received
+	_onChange: function(){
+		this.setState(getAppState());
+	}
 });
 
 module.exports = App;

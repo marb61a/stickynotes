3c0e96df24c89a6544981fbca24cc0fe352658ea
martin byrne <marb61a@gmail.com>
2016-04-17 17:18:14 +0000

    updated addnoteform

--- src/js/components/AddNoteForm.js
+++ src/js/components/AddNoteForm.js
@@ -6,9 +6,28 @@ var AddNoteForm = React.createClass({
     render : function(){
         return(
             <div>
-            
+                <h5>Add a Note</h5>
+                <form onSubmit={this.onSubmit}>
+                    <div className="row">
+                        <div className="large-12 columns">
+							<label>Note Text
+								<input type="text" ref="text" placeholder="Enter Text..." />
+							</label>
+							<button className="button">Add</button>
+						</div>
+                    </div>
+                </form>
             </div>
         );
+    },
+    
+    onSubmit : function(e){
+        e.preventDefault();
+        var note = {
+            text : this.refs.text.value.trim()
+        };
+        
+       AppActions.addNote(note);
     }
 });
 

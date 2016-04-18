b886815b7be15d60ea7e91b1dfd2520d46f95eed
martin byrne <marb61a@gmail.com>
2016-04-18 10:38:00 +0000

    added note component and updated store

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # stickynotes
+You will need to add a valid Mongolab account and details
+to run this app properly.
--- src/js/utils/appAPI.js
+++ src/js/utils/appAPI.js
@@ -1,5 +1,13 @@
 var AppActions = require('../actions/AppActions');
 
 module.exports = {
-	
-}
+    addNote : function(note){
+        $.ajax({
+            url : "",
+            data: JSON.stringify(note),
+			type: "POST",
+			contentType: "application/json"
+        });
+    }
+    
+};
